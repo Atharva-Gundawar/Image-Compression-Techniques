17d42d500bf99d225f51e8e12398d42a7bf06ae6
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-08-12 00:22:02 +0530

    FEAT : compresion and extraction

--- 256RBD_compressor.py
+++ 256RBD_compressor.py
@@ -8,19 +8,24 @@ import sys
 import numpy
 from numpy import ndarray
 
-# numpy.set_printoptions(threshold=sys.maxsize)
-
-img = cv2.imread(r"images\image1.jpg")
-
 
 def decimalToGCM(num):
     out_num = []
-
     def get_quotients(num):
         if num >= 1:
             out_num.append(num % 256)
             get_quotients(num // 256)
     get_quotients(num)
+    if len(out_num)==2:
+        out_num.append(0)
+    elif len(out_num)==1:
+        out_num.append(0)
+        out_num.append(0)
+    elif len(out_num)==0:
+        out_num.append(0)
+        out_num.append(0)
+        out_num.append(0)
+
     return out_num[::-1]
 
 
@@ -29,7 +34,7 @@ def to_GCM(img):
 
 
 def to_numpy(img):
-    ret_img = np.zeros(*img.shape, 3)
+    ret_img = np.zeros((img.shape[0], img.shape[1], 3))
     # grab the image dimensions
     h = img.shape[0]
     w = img.shape[1]
@@ -38,18 +43,14 @@ def to_numpy(img):
     for y in range(0, h):
         for x in range(0, w):
             # threshold the pixel
+            # print(decimalToGCM(img[y, x]))
             ret_img[y, x] = np.array(decimalToGCM(img[y, x]))
 
     # return the thresholded image
     return ret_img
 
 
+img = cv2.imread(r"images\image1.jpg")
 new_img = np.einsum('k,ijk->ij', np.array([256*256, 256, 1]), img)
-
-
-
-
-len_img = len(str(img))
-len_new = len(str(new_img))
-print("Size reduced by :", '{:.1%}'.format(abs(len_img - len_new)/len_img))
-
+reversedimg = to_numpy(new_img)
+print(reversedimg-img)
